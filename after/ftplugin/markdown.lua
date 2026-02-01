4c1c241fe94774f091d2d4ae0b5e2b009bfb0fcf
EdenEast <edenofest@gmail.com>
2026-02-01 02:21:17 -0500

    fix: update markdown filetype options

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,9 +1,15 @@
 -- vim.opt_local.textwidth = 80
+vim.opt_local.concealcursor = "nc"
+vim.opt_local.conceallevel = 2
+vim.opt_local.formatexpr = ""
 vim.opt_local.linebreak = false
 vim.opt_local.spell = true
-vim.opt_local.formatexpr = ""
-vim.opt_local.conceallevel = 2
-vim.opt_local.concealcursor = "nc"
+vim.opt_local.spelllang = { "en_us" }
+
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.textwidth = 120
 
 require("markview").setup({
   experimental = { check_rtp = false },
